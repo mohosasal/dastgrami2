68b189134cff15a87ce5a285e10e3b934bc63773
mohammadhosseinsalimi <MohamadHosein.Salimi@Tapsi.cab>
2025-07-05 21:09:23 +0330

    base structue implemented

---
+++ dasgarmi3/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.jetbrains.kotlin.plugin.jpa") version "1.9.20" // version might vary
+
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.postgresql:postgresql:42.2.18")
+	testImplementation("io.mockk:mockk:1.13.8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+	testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ dasgarmi3/src/main/kotlin/org/example/dasgarmi3/Dasgarmi3Application.kt
@@ -0,0 +1,11 @@
+package org.example.dasgarmi3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Dasgarmi3Application
+
+fun main(args: Array<String>) {
+	runApplication<Dasgarmi3Application>(*args)
+}
